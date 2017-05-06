2fb5efffc2bcd469168238de7e74548349b673a9
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2017-05-07 05:06:11 +0530

    Made changes
    Made changes according to Response Code !!

--- js/AppController.js
+++ js/AppController.js
@@ -3,6 +3,10 @@ app.controller('MainController',function($http,$scope){
 	$scope.getResult = function(){
 
 		console.log($scope.options);
+
+		if ($scope.options == null) {
+			 Materialize.toast('You need to Choose a date !!', 3000, 'rounded');
+		}else{
 		if (isNaN($scope.trainNumber) == true) {
 			console.log($scope.trainNumber);
 			$http.get('http://api.railwayapi.com/name_number/train/'+$scope.trainNumber+"/apikey/gcahw26s")
@@ -10,13 +14,16 @@ app.controller('MainController',function($http,$scope){
 				$scope.data = response.data;
 				console.log(response.data.train.number);
 				console.log(response.data.response_code);
-				if (response.data.response_code === 204) {
-					// window.location.href = "runningstatus.html/?tt="+response.data.train.number+"&dd="+$scope.options;
-					console.log("undefined he yr bc");
+				if (response.data.response_code === 200) {
+					window.location.href = "runningstatus.html/?tt="+response.data.train.number+"&dd="+$scope.options;
+					
+				}else if(response.data.response_code === 204){
+					  Materialize.toast('Add Correct Details!', 3000, 'rounded');
+					  
 				}
 
 			})
-
+}
 		}else{
 			console.log($scope.trainNumber);
 			$http.get('http://api.railwayapi.com/name_number/train/'+$scope.trainNumber+"/apikey/gcahw26s")
@@ -25,9 +32,12 @@ app.controller('MainController',function($http,$scope){
 				console.log(response.data);
 console.log(response.data.response_code);
 
-				if (response.data.response_code === 204) {
+				if (response.data.response_code === 200) {
 										// window.location.href = "runningstatus.html/?tt="+response.data.train.number+"&dd="+$scope.options;
 				console.log("undefined he yr bc");
+				
+				}else if(response.data.response_code === 204){
+					Materialize.toast('Add Correct Details!', 3000, 'rounded');
 				}
 
 
