70322004c227839eb3bdc277b86632aea704763d
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2017-05-08 00:12:51 +0530

    Completed The Basic Prototype
    Completed The Basic Prototype

--- js/AppController.js
+++ js/AppController.js
@@ -15,42 +15,55 @@ app.controller('MainController',function($http,$scope){
 				console.log(response.data.train.number);
 				console.log(response.data.response_code);
 				if (response.data.response_code === 200) {
-					window.location.href = "runningstatus.html/?tt="+response.data.train.number+"&dd="+$scope.options;
-					
-				}else if(response.data.response_code === 204){
-					  Materialize.toast('Add Correct Details!', 3000, 'rounded');
-					  
+					window.location.href = "runningstatus.html?tt="+response.data.train.number+"&dd="+$scope.options;
+					}else if(response.data.response_code === 204){
+					  Materialize.toast('I am a toast!', 3000, 'rounded');	  
 				}
-
 			})
-}
-		}else{
+}		else{
 			console.log($scope.trainNumber);
 			$http.get('http://api.railwayapi.com/name_number/train/'+$scope.trainNumber+"/apikey/gcahw26s")
 			.then(function(response){
 				$scope.data = response.data;
 				console.log(response.data);
-console.log(response.data.response_code);
-
-				if (response.data.response_code === 200) {
-										// window.location.href = "runningstatus.html/?tt="+response.data.train.number+"&dd="+$scope.options;
-				console.log("undefined he yr bc");
-				
-				}else if(response.data.response_code === 204){
-					Materialize.toast('Add Correct Details!', 3000, 'rounded');
-				}
-
-
-
-// $http.get('http://api.railwayapi.com/live/train/'+response.data.train.number+"/doj/"+ $scope.options +"/apikey/gcahw26s/").then(function(response){
-// 				console.log(response.data);
-			})
-
-			
-
+				console.log(response.data.response_code);
+		if (response.data.response_code === 200) {
+										window.location.href = "runningstatus.html?tt="+response.data.train.number+"&dd="+$scope.options;
+						}
+					})		}
 		}
-
-
-
 	}
+});
+
+
+app.controller('LiveTRSController', function($scope,$http){
+ 
+ var preloader = document.getElementById("preloaderid");
+ var trandata = document.getElementById("dataTrainLiveStatus");
+ var trainNumber = getParameterByName("tt");
+ var date = getParameterByName("dd");
+
+ console.log(trainNumber);
+ console.log(date);
+
+ $http.get('http://api.railwayapi.com/live/train/'+trainNumber+"/doj/"+date+"/apikey/gcahw26s/")
+ .then(function(response){
+ 	$scope.data = response.data;
+ 	preloader.style.display = 'none';
+ 	trandata.style.display = 'block';
+ 	console.log($scope.data);
+ 	console.log($scope.data.position);
+ 	$scope.runningheading = $scope.data.position;
+ 	console.log($scope.runningheading);
+
+ 	document.getElementById('trainshortstatus').innerHTML = $scope.runningheading +" ("+$scope.data.current_station.station_.name+")";
+ 	document.getElementById('headingStatus').innerHTML = "Running Status of Train No ("+$scope.data.train_number+") Starting From"+$scope.data.start_date;
+ })
+
+
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
 });
